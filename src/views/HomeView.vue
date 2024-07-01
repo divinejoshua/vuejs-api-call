cf614c1580fadc0c9ed4af0655390b3a3697b309
divinejoshua <divineuche399@gmail.com>
2024-07-01 19:51:30 +0100

    Added API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+//@ts-ignore
+import { onMounted, ref } from 'vue'
+
+
+const info = ref("")
+
+
+onMounted(() => {
+  console.log(`the component is now mounted.`)
+  fetchJokes()
+})
+
+async function fetchJokes() {
+  try {
+    const response = await fetch('https://1k6dp49v7b.execute-api.us-east-1.amazonaws.com/teststage/data');
+    const data = await response.json()
+    info.value = data.body
+    
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    {{info}}
   </main>
 </template>
